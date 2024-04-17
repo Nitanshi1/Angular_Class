310eb620a232adb90f2a8a2eb9a44729ee4bd992
Nitanshi Agarwal <nitanshianshi@gmail.com>
2024-04-17 11:04:29 +0530

    update

---
+++ AngularBackend/Angular_MongoDB/app.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { body , validationResult } = require('express-validator');
+const methodOverride = require('method-override');
+
+const app = express();
+app.use(express.static('public'));
+mongoose.connect('mongodb://localhost:27017/my_database');
+
+const db = mongoose.connection;
+db.on('error' , ()=>{
+    console.log('MongoDb connection error.....');
+})
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    age : Number,
+    email:String
+})
+
+const User = mongoose.model('User',userSchema);
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
+app.set('view engine','ejs');
+app.get('/users/new' , (req,res)=>{
+    res.render('new-user' , {errors:null});
+})
+app.get('/users/:id/edit' ,async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        if(!user){
+            return res.status(404).send('User not found');
+        }
+        res.render('edit-user' , {user , errors:null});
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
+app.get('/users' , async (req,res)=>{
+    try{
+        const users = await User.find();
+        res.render('user-list' , {users})
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
+app.post('/users' ,[
+    body('name').isLength({min:5}).withMessage('Name is required with min 5 chars'),
+    body('age').isInt({min:18}).withMessage('Age must be a number greater than 18'),
+    body('email').isEmail().withMessage('Invalid Email address')
+], async (req,res)=>{
+    console.log(req.body);
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.render('new-user',{errors:errors.array()});
+    }
+    try{
+        const user = new User(req.body);
+        await user.save();
+        res.redirect('/users');
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
+app.put('/users/:id' , [
+    body('name').isLength({min:5}).withMessage('Name is required with min 5 chars'),
+    body('age').isInt({min:18}).withMessage('Age must be a number greater than 18'),
+    body('email').isEmail().withMessage('Invalid Email address')
+], async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const user = await User.findById(req.params.id);
+        return res.render('edit-user',{user , errors:errors.array()});
+    }
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id , req.body, {new:true});
+        if(!user){
+            return res.status(404).send('User not found');
+        }
+        res.redirect('/users');
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
+
+app.delete('/users/:id' , async (req,res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            return res.status(404).send('User not found');
+        }
+        res.redirect('/users');
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
+
+
+const port = 3000;
+app.listen(port , ()=>{
+    console.log(`Server is running on port ${port}....`);
+})
--- AngularBackend/MongoDB/template_JS/app.js
+++ AngularBackend/MongoDB/template_JS/app.js
@@ -1,32 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {body , validationResult} = require('express-validator');
+const multer = require('multer');
+const path = require('path');
+
 const app = express();
 app.set('view engine','ejs');
+app.use(express.static('public'))
+app.use('/uploads' , express.static('uploads'));
 app.use(bodyParser.urlencoded({extended:true}));
-
+const storage = multer.diskStorage({
+    destination : function(req,file,cb){
+        cb(null,'uploads/')
+    },
+    filename: function(req,file,cb){
+        cb(null,Date.now()+path.extname(file.originalname));
+    }
+})
+const uploads = multer({storage:storage});
 app.get('/home' , (req,res)=>{
     res.render('formvalid', {errors:null})
 });
 
-
-app.post('/submit',[
+app.post('/submit', uploads.single('image'),[
     body('name').isLength({min:5}).withMessage('Name is required with min 5 chars'),
-    body('email').isEmail().withMessage('Invalid email')
+    body('email').isEmail().withMessage('Invalid email'),
+    body('image').custom((value , {req})=>{
+        if(!req.file){
+            throw new Error('Image is required');
+        }
+        return true;
+    })
 ],(req,res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         res.render('formvalid' , {errors: errors.array()})
     }
+    const imagePath = req.file.path;
     const {name , email } = req.body;
-    res.render('submissionform' , {name , email});
+   
+    res.render('submissionform' , {name , email, imagePath});
 })
 
-app.post('/submit' , (req,res)=>{
-    const {name, email} =req.body;
-    res.render('submissionform',  {name, email});
-})
 const port = 4000;
 app.listen(port , ()=>{
     console.log(`Server running on port ${port}....`);
 })
+
+
